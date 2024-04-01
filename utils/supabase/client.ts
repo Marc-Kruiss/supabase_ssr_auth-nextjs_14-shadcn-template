f4ce3e5d1aa8e806c61406e9ddae8acf6425678a
Marc Kruiß <k.marc@a1.net>
2024-04-01 17:53:44 +0200

    setup supabase clients (client/server)

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,7 +1,8 @@
+import { Database } from "@/types/supabase";
 import { createBrowserClient } from "@supabase/ssr";
 
 export const createClient = () =>
-  createBrowserClient(
+  createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
